9608084ae381fdccbbc37c648dab2d090791bb90
hidcCombEx <158374721+hidcCombEx@users.noreply.github.com>
2024-02-05 11:11:16 +0100

    More Testing

--- sketch.js
+++ sketch.js
@@ -1,9 +1,46 @@
+let canvas, page = 0
+
 function setup() {
-    createCanvas(windowWidth, windowHeight - 4)
+    canvas = createCanvas(windowWidth, windowHeight - 4)
+    canvas.parent("bg")
+    canvas.style('position: fixed')
+
+    mouseX = width / 2
+    mouseY = height / 2
+
+    let home = select('#home')
+    let about = select('#about')
+    let crews = select('#crews')
+    let projects = select('#projects')
+
+    home.mousePressed(function() { page = 0 })
+    about.mousePressed(function() { page = 1 })
+    crews.mousePressed(function() { page = 2 })
+    projects.mousePressed(function() { page = 3 })
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight - 4)
 }
 
 function draw() {
-    background(240)
+    if (page > 0) {
+        if (height > windowHeight / 4) {
+            resizeCanvas(windowWidth, height / 1.04)
+        }
+    } else {
+        if (height < windowHeight - 4.01) {
+            resizeCanvas(windowWidth, height * 1.04)
+        } else {
+            resizeCanvas(windowWidth, windowHeight - 4)
+        }
+    }
 
-    circle(mouseX, mouseY, 200)
+    background(page == 0? 240:127)
+
+    fill(255)
+    circle(width / 2, height / 2 + map(window.scrollY, 0, 4000, 0, 100), 100)
+
+    fill(0)
+    circle(mouseX, mouseY, 50)
 }

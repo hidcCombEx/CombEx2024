409141a7dd9cb49d703729545bacaa1ad5f11966
hidcCombEx <158374721+hidcCombEx@users.noreply.github.com>
2024-03-10 02:01:26 +0900

    Header Complete

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 document.addEventListener('scroll', () => {
-    const header = document.getElementById("nav")
+    const header = document.getElementById('nav')
 
     if (window.scrollY > 60) header.classList.add('white')
     else header.classList.remove('white')
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.getElementById('toggle')
+
+    toggle.addEventListener('click', function() {
+        const svg = document.querySelector('nav svg path')
+        const menu = document.querySelectorAll('nav ul li')
+
+        if (toggle.classList.contains('on-toggle')) {
+            toggle.classList.remove('on-toggle')
+            svg.style.fill = '#565656'
+            menu.forEach(list => { list.classList.remove('on-menu') })
+        } else {
+            toggle.classList.add('on-toggle')
+            svg.style.fill = '#EA4D37'
+            menu.forEach(list => { list.classList.add('on-menu') })
+        }
+    })
 })
